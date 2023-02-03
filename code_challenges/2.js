08aa8eaeecbd8e84da815b8aaa2fc3bc0547b394
Anneice Manzanares <mountains.stars@gmail.com>
2023-02-02 18:48:33 -0700

    solved 2.js

---
+++ code_challenges/2.js
@@ -0,0 +1,13 @@
+
+
+// Write a function that takes a string as the argument.
+// The function should return a number that is the number of characters in the string.
+// Choose names that make the code easy to understand.
+
+function countChars (str) {
+   return str.length;
+}
+
+console.log(countChars('hello'));
+console.log(countChars('hi Cristina'))
+console.log(countChars('hi Sarah'))
